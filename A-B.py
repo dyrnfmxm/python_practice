36d31bcef213fb0ffeb0d9ee1b23c65bf0f25b3b
dyrnfmxm <dyrnfmxm9@gmail.com>
2020-06-02 21:42:48 +0900

    Add files via upload

---
+++ A-B.py
@@ -0,0 +1,4 @@
+a = int(input("첫 번째 정수 입력 "))
+b = int(input("두 번째 정수 입력 "))
+
+print("두 수의 차" ,a-b)
---
+++ cal.py
@@ -0,0 +1,8 @@
+a = int(input("첫 번째 정수 입력 "))
+b = int(input("두 번째 정수 입력 "))
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
