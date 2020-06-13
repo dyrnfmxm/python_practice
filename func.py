45b3a41cfd45edcb5ef31ad5de4724647823b96b
dyrnfmxm <dyrnfmxm9@gmail.com>
2020-06-13 20:57:43 +0900

    Add files via upload

---
+++ func.py
@@ -0,0 +1,4 @@
+def add_num2():
+    a, b, c, d = map(int,input("4개의 숫자 입력").split())
+    result = a+b+c+d
+    return(result)
---
+++ func_sum.py
@@ -0,0 +1,5 @@
+def solve(list):
+    result = 0
+    for num in list:
+        result += num
+    return result
