fa8e43e4c4571803247d31b643ec3b3d2bb36fc8
dyrnfmxm <dyrnfmxm9@gmail.com>
2020-07-11 12:34:42 +0900

    Add files via upload

---
+++ codeUp_1067.py
@@ -0,0 +1,13 @@
+n = int(input())
+if n > 0:
+    print("plus")
+    if n%2 == 0:
+        print("even")
+    else:
+        print("odd")
+else:
+    print("minus")
+    if n%2 == 0:
+        print("even")
+    else:
+        print("odd")
