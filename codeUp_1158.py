7030e1b97e3be24d42854399aea34a42b9969bc2
dyrnfmxm <dyrnfmxm9@gmail.com>
2020-07-25 10:57:31 +0900

    Add files via upload

---
+++ codeUp_1158.py
@@ -0,0 +1,7 @@
+n = int(input())
+if n >= 60 and n <= 70:
+    print("win")
+elif n >=30 and n<=40:
+    print("win")
+else:
+    print("lose")
---
+++ codeUp_1159.py
@@ -0,0 +1,7 @@
+n = int(input())
+if n >= 50 and n <= 70:
+    print("win")
+elif n % 6 == 0:
+    print("win")
+else:
+    print("lose")
