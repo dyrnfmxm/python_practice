53a836727a22e84916ff36b00ed4e04e05463b22
dyrnfmxm <dyrnfmxm9@gmail.com>
2020-10-18 18:46:45 +0900

    Add files via upload

---
+++ 시각.py
@@ -0,0 +1,12 @@
+h = int(input())
+
+count = 0
+for i in range(h+1):
+    for j in range(60):
+        for k in range(60):
+            if '3' in str(i) + str(j) + str(k):
+                count += 1
+
+print(count)
+
+# 00시 00분부터 n시까지 3이 들어있으면 숫자 세기
---
+++ 왕실의나이트.py
@@ -0,0 +1,19 @@
+# 현재 위치
+input_data = input()
+row = int(input_data[1])
+column = int(ord(input_data[0])) - int(ord('a'))+1
+
+# 이동 가능한 8가지 방향 정의
+steps = [(-2,-1), (-1,-2), (1,-2), (2,-1), (2,1), (1,2), (-1,2), (-2,1)]
+
+# 각 방향에 대해 이동이 가능한지 확인
+result = 0
+for step in steps:
+    # 이동하고자 하는 위치 확인
+    next_row = row + step[0]
+    next_column = column + step[1]
+    # 해당 위치로 이동 가능하면 카운트 증가
+    if next_row >= 1 and next_row <= 8 and next_column >= 1 and next_column <= 8:
+        result += 1
+
+print(result)
