0707e1235eb56568b418a03c62c07b83b6c9b939
dyrnfmxm <dyrnfmxm9@gmail.com>
2021-08-17 07:45:33 +0900

    Create number_k.py

---
+++ number_k.py
@@ -0,0 +1,20 @@
+def solution(array, commands):
+
+    answer = []
+
+    for i in commands:
+
+        a, b, c = i[0]-1, i[1], i[2]-1
+
+        arr = array[a:b]
+
+        arr.sort()
+
+        
+
+        answer.append(arr[c])
+
+        
+
+    return answer
+
