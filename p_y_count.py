3155bfd7c6bf578f01ab1f90e67b1a164c8ff9dd
dyrnfmxm <dyrnfmxm9@gmail.com>
2021-09-02 23:10:34 +0900

    Add files via upload

---
+++ p_y_count.py
@@ -0,0 +1,12 @@
+def solution(s):
+    p_count = 0
+    y_count = 0
+    for i in s:
+        if i == 'p' or i == 'P':
+            p_count += 1
+        elif i == 'y' or i == 'Y':
+            y_count += 1
+    if p_count == y_count:
+        return True
+    else:
+        return False
