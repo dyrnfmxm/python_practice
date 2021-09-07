59f1de2152b22ba8f31da0d75c9d0eb96fccf953
dyrnfmxm <dyrnfmxm9@gmail.com>
2021-09-07 23:21:48 +0900

    Add files via upload

---
+++ not_finish_the_course_a.py
@@ -0,0 +1,9 @@
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+    print(participant, completion)
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            return participant[i]
+        elif i == len(completion)-1:
+            return participant[i+1]
