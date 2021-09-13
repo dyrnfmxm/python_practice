e6f5067ff12dbb6c1db53acc5bd7bffac975bb8c
dyrnfmxm <dyrnfmxm9@gmail.com>
2021-09-13 23:15:45 +0900

    Add files via upload

---
+++ jaden_case.py
@@ -0,0 +1,14 @@
+def solution(s):
+    index = 0
+    ss = ''
+    for i in s:
+        if i == " ":
+            index = 0
+        elif index == 0:
+            i = i.upper()
+            index += 1
+        else:
+            i = i.lower()
+            index += 1
+        ss += i
+    return ss
