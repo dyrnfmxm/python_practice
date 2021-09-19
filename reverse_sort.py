9297cc8649e39ab20af6e10a46214914becd78fa
dyrnfmxm <dyrnfmxm9@gmail.com>
2021-09-19 23:45:11 +0900

    Add files via upload

---
+++ reverse_sort.py
@@ -0,0 +1,10 @@
+def solution(n):
+    a = str(n)
+    arr = []
+    for i in range(len(a)):
+        arr.append(a[i])
+    arr.sort(reverse = True)
+    answer = ''
+    for i in arr:
+        answer += i
+    return int(answer)
