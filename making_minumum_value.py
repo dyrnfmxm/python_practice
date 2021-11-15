aa13e0a86a289f63be549f37b06698e4337d1445
dyrnfmxm <dyrnfmxm9@gmail.com>
2021-11-15 21:55:23 +0900

    Update making_minumum_value.py

--- making_minumum_value.py
+++ making_minumum_value.py
@@ -6,7 +6,7 @@ def solution(A,B):
     # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
     
     for i in range(len(A)):
-        print(A[i]*B[i])
+        #print(A[i]*B[i])
         answer += A[i] * B[i]
 
     return answer
