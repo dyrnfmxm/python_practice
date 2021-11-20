889bc962b5c1622ec537200d9172f3170805714c
dyrnfmxm <dyrnfmxm9@gmail.com>
2021-11-20 23:32:33 +0900

    Update making_minumum_value.py

--- making_minumum_value.py
+++ making_minumum_value.py
@@ -3,7 +3,7 @@ def solution(A,B):
     answer = 0
     A.sort()
     B.sort(reverse=True)
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다
+    # [실행] 버튼을 누르면 출력 값을 볼 수 있습
     
     for i in range(len(A)):
         #print(A[i]*B[i])
